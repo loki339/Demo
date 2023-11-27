518fa19121b358a326c9a1440dfc997ac9d0c743
loki339 <142960043+loki339@users.noreply.github.com>
2023-11-27 11:43:09 -0600

    Update App.test.js

--- caronx/src/App.test.js
+++ caronx/src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App.js from './App.js';
 
 test('renders learn react link', () => {
-  render(<App/>);
+  render(<App.js/>);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
