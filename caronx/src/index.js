c71634f7a5d05e4ffa7bfd2f943d441f59ad7327
loki339 <142960043+loki339@users.noreply.github.com>
2023-11-27 12:39:05 -0600

    Update index.js

--- caronx/src/index.js
+++ caronx/src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import "./App.js";
-import "./reportWebVitals.js";
+import App from "./App.js";
+import reportWebVitals from "./reportWebVitals.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
